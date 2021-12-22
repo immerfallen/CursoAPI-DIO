7e0306e350b4ea6fe9aa974d5b50165d8aa53f0b
Maro de Melo <maromelo@tempus.digital>
2021-12-22 16:26:55 -0300

    Add project files.

---
+++ Curso.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,40 @@
+﻿using Curso.Api.Models;
+using Curso.Api.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Curso.Api.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+
+        /// <summary>
+        /// teste
+        /// </summary>
+        /// <param name="loginViewModelInput"></param>
+        /// <returns></returns>
+        [SwaggerResponse(statusCode:200, description: "Sucesso ao autenticar", Type= typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Campos obrigatórios", Type= typeof(ValidaCampoViewModelOutput))]
+        [SwaggerResponse(statusCode: 500, description: "Erro interno", Type= typeof(ErroGenericoViewModel))]
+        [HttpPost]
+        [Route("logar")]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Ok(loginViewModelInput);
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        public IActionResult Registrar(RegistroViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+    }
+}
---
+++ Curso.Api/Models/Usuarios/RegistroViewModelInput.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Curso.Api.Models.Usuarios
+{
+
+    public class RegistroViewModelInput
+    {
+        [Required(ErrorMessage = "O Login é obrigatório.")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "O Email é obrigatório.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        public string Senha { get; set; }
+    }
+}
